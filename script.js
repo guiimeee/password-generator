1033ace145a10a32c0a1420c7e2f62adffb1aed8
guiimeee <guilhermeilhaer@gmail.com>
2025-03-27 11:16:52 -0300

    teste1

---
+++ script.js
@@ -0,0 +1,34 @@
+let sizePassword = document.querySelector('#valor')
+let sliderElement = document.querySelector('#slider')
+let button = document.querySelector('#button')
+
+let container = document.querySelector('.generated-password')
+let password = document.querySelector('#password')
+let copy = document.querySelector('#copy')
+
+let charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=<>?/.,:;{}[]|\\`~ '
+
+let novaSenha = ''
+
+sizePassword.innerText = sliderElement.value
+
+sliderElement.oninput = function() {
+    sizePassword.innerHTML = this.value
+}
+
+button.addEventListener('click', function generatePassword() {
+    let pass = ""
+
+    for(let i = 0, n = charset.length; i < sliderElement.value; i++) {
+        pass += charset.charAt(Math.floor(Math.random() * n))
+    }
+
+    container.classList.remove('hide')
+    password.innerText = pass
+    novaSenha = pass
+})
+
+password.addEventListener('click', () => {
+    alert('Senha copiada com sucesso!')
+    navigator.clipboard.writeText(novaSenha)
+})
